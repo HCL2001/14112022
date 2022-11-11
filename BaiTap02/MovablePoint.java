dcf680bae20dfd367874b39ffe04c4b3fe6f673d
Long Hoang <longhoang12212@gmail.com>
2022-11-11 10:36:57 +0700

    BaiTap02 pack

---
+++ BaiTap02/MovablePoint.java
@@ -0,0 +1,42 @@
+package BaiTap02;
+
+public class MovablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed){
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float ySpeed){
+        super(x, y);
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public float[] getSpeed(){
+        float arr[] = {this.xSpeed, this.ySpeed};
+        return arr;
+    }
+}
---
+++ BaiTap02/Test.java
@@ -0,0 +1,10 @@
+package BaiTap02;
+
+public class Test {
+    public static void main(String[] args) {
+        Point p = new Point(1, 1);
+        MovablePoint pMove = new MovablePoint(1, 1, 2, 2);
+        System.out.println(p);
+        System.out.println(pMove.toString());
+    }
+}
